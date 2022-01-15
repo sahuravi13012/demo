42ec399d6aae4c80c75976be815b9ea4dfdc34ad
sahuravi13012 <sahuravi13012@gmail.com>
2022-01-15 14:15:05 +0530

    this is five  commit

---
+++ mobile.js
@@ -0,0 +1,10 @@
+var moble = {
+  moble_name: "lenvo k6 power",
+  model_No: 215,
+  company: "lenvo",
+  RAM: "2GB",
+  storage: "32GB",
+  Processor: "8 core 1.4hz",
+  Android_version: 7.0,
+};
+console.log(moble.model_No);
