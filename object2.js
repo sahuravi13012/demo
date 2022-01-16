b6910c570d8880469cba5093aac8a516157ae6ba
sahuravi13012 <sahuravi13012@gmail.com>
2022-01-16 11:06:57 +0530

    this is sixth commit

---
+++ object2.js
@@ -0,0 +1,19 @@
+var array = [
+  {
+    studenName: "Ravi sahu",
+    RollNo: 215469215,
+    Department: "CSE",
+    Semester: "Third",
+  },
+
+  {
+    moble_name: "lenvo k6 power",
+    model_No: 215,
+    company: "lenvo",
+    RAM: "2GB",
+    storage: "32GB",
+    Processor: "8 core 1.4hz",
+    Android_version: 7.0,
+  },
+];
+console.log(array[1].Processor);
